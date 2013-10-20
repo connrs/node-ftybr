a656916552df53d8377f184adf907a241a37a01e
Paul Connolley <paul.connolley@gmail.com>
2013-10-20 12:53:47 +0100

    Fixes for issue #1 and issue #2

--- test/ftybr.js
+++ test/ftybr.js
@@ -20,6 +20,7 @@ function initRouter() {
 function resetReqRes() {
   req = new PassThrough();
   res = new PassThrough();
+  res.statusCode = 200;
 }
 
 test('Expose Constructor', function (t) {
@@ -136,4 +137,124 @@ test('Use middleware', function (t) {
   });
   r.requestListener(req, res);
   t.equal(req.funkmeister, true);
+});
+
+test('500 Error if middleware error', function (t) {
+  var fakeController = {
+    getRoutes: function () { return [
+      ['get', '/', function (req, res) {
+        res.end('SUCCESSINATOR');
+      }]
+    ]; }
+  };
+  t.plan(1);
+  initRouter();
+  resetReqRes();
+  r.registerMiddleware(function (req, res, done) {
+    done(new Error('MIDDLEWARE ERROR'));
+  });
+  r.registerController(fakeController);
+  r.requestListener(req, res);
+  t.equal(res.statusCode, 500);
+});
+
+test('Custom middleware error if status code set', function (t) {
+  var fakeController = {
+    getRoutes: function () { return [
+      ['get', '/', function (req, res) {
+        res.end('SUCCESSINATOR');
+      }]
+    ]; }
+  };
+  t.plan(2);
+  initRouter();
+  resetReqRes();
+  r.registerMiddleware(function (req, res, done) {
+    res.statusCode = 400;
+    done(new Error('MIDDLEWARE ERROR'));
+  });
+  r.registerController(fakeController);
+  collectStream(res, function (err, data) {
+    t.equal(data, 'Bad Request');
+  });
+  r.requestListener(req, res);
+  t.equal(res.statusCode, 400);
+});
+
+test('Error passed to error handler', function (t) {
+  var fakeController = {
+    getRoutes: function () { return [
+      ['get', '/', function (req, res) {
+        res.end('SUCCESSINATOR');
+      }]
+    ]; }
+  };
+  var fakeErrorController = {
+    getRoutes: function () { return [
+      ['500', function (req, res, err) {
+        t.equal(err.message, 'MIDDLEWARE ERROR');
+      }]
+    ]; }
+  };
+  t.plan(1);
+  initRouter();
+  resetReqRes();
+  r.registerMiddleware(function (req, res, done) {
+    done(new Error('MIDDLEWARE ERROR'));
+  });
+  r.registerController(fakeController);
+  r.registerErrorController(fakeErrorController);
+  r.requestListener(req, res);
+});
+
+test('Error message passed to error handler', function (t) {
+  var fakeController = {
+    getRoutes: function () { return [
+      ['get', '/', function (req, res) {
+      console.log('here');
+        res.render500(new Error('erm'), 'Helpful text');
+      }]
+    ]; }
+  };
+  var fakeErrorController = {
+    getRoutes: function () { return [
+      ['500', function (req, res, err, context) {
+        t.equal(context.message, 'Helpful text');
+      }]
+    ]; }
+  };
+  t.plan(1);
+  initRouter();
+  resetReqRes();
+  req.url = '/';
+  req.method = 'GET';
+  r.registerController(fakeController);
+  r.registerErrorController(fakeErrorController);
+  r.requestListener(req, res);
+});
+
+test('Context object passed to error handler', function (t) {
+  var fakeController = {
+    getRoutes: function () { return [
+      ['get', '/', function (req, res) {
+      console.log('here');
+        res.render500(new Error('erm'), { data: 1 });
+      }]
+    ]; }
+  };
+  var fakeErrorController = {
+    getRoutes: function () { return [
+      ['500', function (req, res, err, context) {
+        t.equal(context.data, 1);
+      }]
+    ]; }
+  };
+  t.plan(1);
+  initRouter();
+  resetReqRes();
+  req.url = '/';
+  req.method = 'GET';
+  r.registerController(fakeController);
+  r.registerErrorController(fakeErrorController);
+  r.requestListener(req, res);
 });
